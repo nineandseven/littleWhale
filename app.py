2872dd7c96fdd4d25cc1fac164a383942be29018
devfromweiren <devfromweiren@gmail.com>
2024-06-22 15:19:42 +0800

    close auto-reply

--- app.py
+++ app.py
@@ -18,16 +18,16 @@ handler = WebhookHandler('5b866245fcdd912ac77b413dd1b637d9')
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    # # get X-Line-Signature header value
+    # signature = request.headers['X-Line-Signature']
+    # # get request body as text
+    # body = request.get_data(as_text=True)
+    # app.logger.info("Request body: " + body)
+    # # handle webhook body
+    # try:
+    #     handler.handle(body, signature)
+    # except InvalidSignatureError:
+    #     abort(400)
     return 'OK'
 
 # 處理訊息
